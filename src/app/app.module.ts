62ab2ac8a8758bc5b45a220bc66fd451f15377a1
MarcoSuess <81395283+MarcoSuess@users.noreply.github.com>
2021-07-22 15:09:54 +0200

    First playable version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { PlayerComponent } from './player/player.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { DialogAddPlayerComponent } from './dialog-add-player/dialog-add-player.component';
+import { DialogOverviewExampleDialog } from './dialog-add-player/dialog-add-player.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
@@ -27,7 +27,7 @@ import { AngularFireModule } from '@angular/fire';
     StartScreenComponent,
     GameComponent,
     PlayerComponent,
-    DialogAddPlayerComponent,
+    DialogOverviewExampleDialog,
     GameInfoComponent,
  /*    GameInfoComponent,
     PlayerMobileComponent,
